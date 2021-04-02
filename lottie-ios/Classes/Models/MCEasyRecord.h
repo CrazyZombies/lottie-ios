7772882b100e1f0fcb7a5dba0552a7da7edb7df5
senquancai <senquancai@tencent.com>
2021-04-02 18:18:45 +0800

    test code;

---
+++ lottie-ios/Classes/Models/MCEasyRecord.h
@@ -0,0 +1,19 @@
+//
+//  MCEasyRecord.h
+//  AFNetworking iOS
+//
+//  Created by csq on 2021/4/2.
+//  Copyright © 2021 AFNetworking. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MCEasyRecord : NSObject
+
++ (void)recordLog;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ lottie-ios/Classes/Models/MCEasyRecord.m
@@ -0,0 +1,17 @@
+//
+//  MCEasyRecord.m
+//  AFNetworking iOS
+//
+//  Created by csq on 2021/4/2.
+//  Copyright © 2021 AFNetworking. All rights reserved.
+//
+
+#import "MCEasyRecord.h"
+
+@implementation MCEasyRecord
+
++ (void)recordLog {
+    NSLog(@"MCEasyRecord -- recordLog");
+}
+
+@end
